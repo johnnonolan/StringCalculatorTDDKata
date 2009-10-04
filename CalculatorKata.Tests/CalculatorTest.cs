e0d75b1a70aa4066b92563f0a732783c70a8de77
Johnno Nolan <johnnonolan@yahoo.co.uk>
2009-10-04 13:49:43 +0100

    Initial import into repo

---
+++ CalculatorKata.Tests/CalculatorTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CalculatorKata.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            //Arrange
+            var calc = new Calculator();
+            //Act
+            var result= calc.Add("");
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void Add_Number_ReturnsNumber()
+        {
+            
+            //Arrange
+            var input = 4.01m;             
+            //Act
+            var calc = new Calculator();
+            var result = calc.Add(input);
+            //Assert
+            Assert.AreEqual(input, result);
+        }
+
+        //Takes 2 numbers separated by delimeter (',') and returns sum
+        [Test]
+        public void Add_2NumbersSeperatedByCommaDelimiter_ReturnsSum()
+        {
+            //Arrange
+            var input = "3,2";
+            //Act
+            var calc = new Calculator();
+            var result = calc.Add(input);
+            //Assert
+            Assert.AreEqual(5, result);            
+        }
+        
+  
+        [Test]
+        public void Add_MulitpleNumbers_ReturnSum()
+        {
+            //Arrange
+            var input = "3,2,8.5";
+            //Act
+            var calc = new Calculator();
+            var result = calc.Add(input);
+            //Assert
+            Assert.AreEqual(3+2+8.5, result);            
+
+        }
+
+        [Test]
+        public void Add_When_ChangeDelimiter()
+        {
+            var input = "3,2,8.5";
+            //Act
+            var calc = new Calculator();
+            var result1 = calc.Add(input);
+            calc.ChangeDelimiter('/');
+            var input2 = "3/2/8.5";
+            var result2 = calc.Add(input2);
+            Assert.AreEqual(result1,result2);
+        }
+
+    }
+}
---
+++ CalculatorKata/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorKata
+{
+    public class Calculator
+    {
+        private char _delimiter = ',';
+        public decimal Add(string input)
+        {
+            if (input == String.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                decimal total = 0.00M;
+                var inputs =input.Split(_delimiter);
+                foreach (string s in inputs)
+                {
+                    Decimal decimalInput;
+                    Decimal.TryParse(s, out decimalInput);
+                    total += decimalInput;
+                }
+                return total;                                   
+            }
+        }
+
+        public decimal Add(decimal input)
+        {
+            return input;
+        }
+
+        public void ChangeDelimiter(char delimimeter)
+        {
+            _delimiter = delimimeter;
+        }
+    }
+}
