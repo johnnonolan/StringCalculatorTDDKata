a918d18641c6452b54b57d91ca5e1c73f8749ac9
Johnno Nolan <johnnonolan@yahoo.co.uk>
2009-10-09 23:26:29 +0100

    5th Kata tried to use CodeRush xpress more. It seems to take me 8 mins to get started

--- CalculatorKata/Calculator.cs
+++ CalculatorKata/Calculator.cs
@@ -1,42 +1,70 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+
 
 namespace CalculatorKata
 {
+
     public class Calculator
     {
-        private const char _delimiter = ',';
-
         public Calculator()
         {
-
         }
-        public int Add(string numbers)
+        
+        private char _Delimiter = ',';
+
+        private string ChangeDelimterAndRemoveCommand(string numberString)
         {
-            if (numbers.Contains(_delimiter))
+            var changeDelimiterCommand = @"//";
+            if (numberString.Contains(changeDelimiterCommand))
             {
-                var result = 0;
-                var numbersCollection = numbers.Split(_delimiter);
-                foreach (var number in numbersCollection)
-                {
-                    result += ParseNumber(number);
-                }
-                return result;
-            } 
+                _Delimiter = numberString.Substring(3, 1).ToCharArray()[0];
+                numberString = numberString.Remove(0, 3);              
+            }
+            return numberString;
 
-            return ParseNumber(numbers);
+        }
+        public int Add(string numberString)
+        {
+            if (String.IsNullOrEmpty(numberString))
+                return 0;
+            numberString = ChangeDelimterAndRemoveCommand(numberString);
+            
+            if (numberString.Contains(_Delimiter))
+            {
+                return AddManyNumbers(numberString, _Delimiter);
+            }
+            return ExtractNumberFromString(numberString);
         }
 
-        private static int ParseNumber(string numbers)
+        private static int AddManyNumbers(string numberString, char delimiter)
         {
-            const int DEFAULTVALUE = 0;
-            int returnInt = 0;
-            Int32.TryParse(numbers, out returnInt);
 
+            numberString = ReplaceNewLinesWithDelimiter(numberString, delimiter);
+            var speraratedNumbers = numberString.Split(delimiter);
+            var total = 0;
+            foreach (var number in speraratedNumbers)
+            {
+                total += ExtractNumberFromString(number);
+	        }
+            return total;
+        }
 
-            return String.IsNullOrEmpty(numbers) ? DEFAULTVALUE : returnInt;
+        private static string ReplaceNewLinesWithDelimiter(string numberString, char delimiter)
+        {
+            numberString = numberString.Replace('\n', delimiter);
+            return numberString;
+        }
+
+        private static int ExtractNumberFromString(string numberString)
+        {
+            var resultInt = 0;
+            if (Int32.TryParse(numberString, out resultInt))
+                return resultInt;
+            throw new FormatException(numberString);
         }
     }
 }
